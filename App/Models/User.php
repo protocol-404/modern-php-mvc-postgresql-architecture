19ea2c9b9cda45a5c1847d379143536b8ef23dc3
9asdaoui <oussamaqasdaoui@gmail.com>
2025-02-04 10:38:36 +0100

    Add User model with properties and constructor; update composer.json to include vlucas/phpdotenv

--- App/Models/User.php
+++ App/Models/User.php
@@ -1,5 +1,16 @@
 <?php
 
 class User {
-    
+    private int $id;
+    private string $username;
+    private string $email;
+    private string $password;
+
+
+    public function __construct(int $id, string $username, string $email, string $password) {
+        $this->id = $id;
+        $this->username = $username;
+        $this->email = $email;
+        $this->password = $password;
+    }
 }
